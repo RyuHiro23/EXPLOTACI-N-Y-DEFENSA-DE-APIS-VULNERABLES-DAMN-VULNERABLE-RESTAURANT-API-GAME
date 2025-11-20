a5a98712c0bc168c2cbd77789f7d9a3463e69071
Ryu <Diksoncantillo290@gmail.com>
2025-11-19 23:38:55 -0500

    Add files via upload

---
+++ NIvel3Corregido.py
@@ -0,0 +1,7 @@
+[Desktop Entry]
+Version=1.0
+Type=Link
+Name=NIvel3Corregido
+Comment=
+Icon=application-archive
+URL=file:///home/kali/Universidad/semestre8/optativa/proyecto_final_Hecho/Damn-Vulnerable-RESTaurant-API-Game/app/apis/users/services/update_user_role_service.py
